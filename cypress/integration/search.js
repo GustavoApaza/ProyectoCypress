5f6f40eb73cad48a0d3076f2e309d934900fdf96
Gustavo Apaza Huanca <Gustavo.ApazaHuanca@endava.com>
2022-03-01 14:18:39 -0300

    Se agrego un comand para el search

--- cypress/integration/search.js
+++ cypress/integration/search.js
@@ -3,19 +3,13 @@ describe('Search elements', () => {
         cy.visit('/')//La barra '/' toma por defecto lo que esta en el archivo cypress.json (baseUrl...)
     })
     it('Search for elments with multiple results', () => {
-        cy.fixture('index').then((index) => {
-            cy.get(index.searchBox).type('dress');
-            cy.get(index.searchButton).click();
-        })
+        cy.search('dress');
         cy.fixture('searchResult').then((searchResult) => {
             cy.get(searchResult.title).should('contain', '"dress"');
         })
     })
     it('Search for elments with no results', () => {
-        cy.fixture('index').then((index) => {
-            cy.get(index.searchBox).type('apple');
-            cy.get(index.searchButton).click();
-        })
+        cy.search('kiwi');
         cy.fixture('searchResult').then((searchResult) => {
             cy.get(searchResult.aviso).should('contain', 'No results were found for your search');
         })
